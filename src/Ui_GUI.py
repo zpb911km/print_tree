2814d575e0a87d45ce615b46f936d3777be63e73
K926v3C <K926v3C@protonmail.com>
2022-12-31 17:39:33 +0800

    added the function of auto-transforming in the GUI

--- src/Ui_GUI.py
+++ src/Ui_GUI.py
@@ -17,64 +17,132 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(857, 710)
-        self.horizontalLayout = QHBoxLayout(MainWindow)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        MainWindow.resize(1024, 720)
+        self.horizontalLayout_2 = QHBoxLayout(MainWindow)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.horizontalSpacer = QSpacerItem(20, 20, QSizePolicy.Preferred, QSizePolicy.Minimum)
 
-        self.horizontalLayout.addItem(self.horizontalSpacer)
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
 
-        self.InputPart = QVBoxLayout()
-        self.InputPart.setObjectName(u"InputPart")
-        self.verticalSpacer = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Preferred)
+        self.InputLayout = QVBoxLayout()
+        self.InputLayout.setObjectName(u"InputLayout")
+        self.verticalSpacer_2 = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Preferred)
 
-        self.InputPart.addItem(self.verticalSpacer)
+        self.InputLayout.addItem(self.verticalSpacer_2)
 
         self.InputBox = QPlainTextEdit(MainWindow)
         self.InputBox.setObjectName(u"InputBox")
+        font = QFont()
+        font.setFamily(u"Cascadia Mono")
+        self.InputBox.setFont(font)
         self.InputBox.viewport().setProperty("cursor", QCursor(Qt.IBeamCursor))
 
-        self.InputPart.addWidget(self.InputBox)
+        self.InputLayout.addWidget(self.InputBox)
 
-        self.verticalSpacer_2 = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Preferred)
+        self.verticalSpacer = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Preferred)
 
-        self.InputPart.addItem(self.verticalSpacer_2)
+        self.InputLayout.addItem(self.verticalSpacer)
 
 
-        self.horizontalLayout.addLayout(self.InputPart)
+        self.horizontalLayout_2.addLayout(self.InputLayout)
 
-        self.TransformationButton = QPushButton(MainWindow)
+        self.horizontalSpacer_3 = QSpacerItem(10, 20, QSizePolicy.Preferred, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_3)
+
+        self.OperationLayout = QVBoxLayout()
+        self.OperationLayout.setObjectName(u"OperationLayout")
+        self.verticalSpacer_5 = QSpacerItem(20, 250, QSizePolicy.Minimum, QSizePolicy.Preferred)
+
+        self.OperationLayout.addItem(self.verticalSpacer_5)
+
+        self.ButtonGroup_1 = QGroupBox(MainWindow)
+        self.ButtonGroup_1.setObjectName(u"ButtonGroup_1")
+        self.verticalLayout = QVBoxLayout(self.ButtonGroup_1)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.AutoTransCheckBox = QCheckBox(self.ButtonGroup_1)
+        self.AutoTransCheckBox.setObjectName(u"AutoTransCheckBox")
+        self.AutoTransCheckBox.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.verticalLayout.addWidget(self.AutoTransCheckBox)
+
+        self.TransformationButton = QPushButton(self.ButtonGroup_1)
         self.TransformationButton.setObjectName(u"TransformationButton")
         self.TransformationButton.setCursor(QCursor(Qt.PointingHandCursor))
 
-        self.horizontalLayout.addWidget(self.TransformationButton)
+        self.verticalLayout.addWidget(self.TransformationButton)
 
-        self.OutputPart = QVBoxLayout()
-        self.OutputPart.setObjectName(u"OutputPart")
+
+        self.OperationLayout.addWidget(self.ButtonGroup_1)
+
+        self.verticalSpacer_7 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Preferred)
+
+        self.OperationLayout.addItem(self.verticalSpacer_7)
+
+        self.ButtonGroup_2 = QGroupBox(MainWindow)
+        self.ButtonGroup_2.setObjectName(u"ButtonGroup_2")
+        self.verticalLayout_3 = QVBoxLayout(self.ButtonGroup_2)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.checkBox = QCheckBox(self.ButtonGroup_2)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setEnabled(False)
+        self.checkBox.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.verticalLayout_3.addWidget(self.checkBox)
+
+        self.spinBox = QSpinBox(self.ButtonGroup_2)
+        self.spinBox.setObjectName(u"spinBox")
+        self.spinBox.setMinimum(1)
+        self.spinBox.setMaximum(2147483647)
+        self.spinBox.setValue(30)
+
+        self.verticalLayout_3.addWidget(self.spinBox)
+
+
+        self.OperationLayout.addWidget(self.ButtonGroup_2)
+
+        self.verticalSpacer_6 = QSpacerItem(20, 250, QSizePolicy.Minimum, QSizePolicy.Preferred)
+
+        self.OperationLayout.addItem(self.verticalSpacer_6)
+
+
+        self.horizontalLayout_2.addLayout(self.OperationLayout)
+
+        self.horizontalSpacer_4 = QSpacerItem(10, 20, QSizePolicy.Preferred, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_4)
+
+        self.OutputLayout = QVBoxLayout()
+        self.OutputLayout.setObjectName(u"OutputLayout")
         self.verticalSpacer_3 = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Preferred)
 
-        self.OutputPart.addItem(self.verticalSpacer_3)
+        self.OutputLayout.addItem(self.verticalSpacer_3)
 
         self.OutputBox = QPlainTextEdit(MainWindow)
         self.OutputBox.setObjectName(u"OutputBox")
+        font1 = QFont()
+        font1.setFamily(u"Cascadia Code")
+        self.OutputBox.setFont(font1)
         self.OutputBox.viewport().setProperty("cursor", QCursor(Qt.IBeamCursor))
         self.OutputBox.setReadOnly(True)
 
-        self.OutputPart.addWidget(self.OutputBox)
+        self.OutputLayout.addWidget(self.OutputBox)
 
         self.verticalSpacer_4 = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Preferred)
 
-        self.OutputPart.addItem(self.verticalSpacer_4)
+        self.OutputLayout.addItem(self.verticalSpacer_4)
 
 
-        self.horizontalLayout.addLayout(self.OutputPart)
+        self.horizontalLayout_2.addLayout(self.OutputLayout)
 
         self.horizontalSpacer_2 = QSpacerItem(20, 20, QSizePolicy.Preferred, QSizePolicy.Minimum)
 
-        self.horizontalLayout.addItem(self.horizontalSpacer_2)
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
 
 
         self.retranslateUi(MainWindow)
+        self.checkBox.toggled.connect(self.spinBox.setEnabled)
+        self.AutoTransCheckBox.toggled.connect(self.TransformationButton.setDisabled)
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -82,6 +150,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Form", None))
         self.InputBox.setPlaceholderText(QCoreApplication.translate("MainWindow", u"\u5728\u6b64\u8f93\u5165\u2026\u2026", None))
+        self.AutoTransCheckBox.setText(QCoreApplication.translate("MainWindow", u"\u81ea\u52a8\u8f6c\u6362", None))
         self.TransformationButton.setText(QCoreApplication.translate("MainWindow", u"\u8f6c\u6362", None))
+        self.checkBox.setText(QCoreApplication.translate("MainWindow", u"\u957f\u5ea6\u9650\u5236", None))
     # retranslateUi
 
